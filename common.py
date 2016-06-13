7698db1175017e35a6ac014f74fb905ac7301f31
makaimark <makaimark@gmail.com>
2016-06-13 21:53:30 +0200

    generate_random 1.0

--- common.py
+++ common.py
@@ -2,7 +2,7 @@
 
 import random
 
-
+table = ['h', 'k', 'hhh']
 # generate and return a unique and random string
 # other expectation:
 # - at least 2 special char()expect: ';'), 2 number, 2 lower and 2 upper case letter
@@ -10,10 +10,26 @@ import random
 #
 # @table: list of list
 # @generated: string - generated random string (unique in the @table)
+
+
 def generate_random(table):
-
+    result = ""
     generated = ''
+    i = 0
 
-    # your code
+    while True:
+        generated = generated + random.choice("abcdefghijklmnopqrstuvwxyz")
+        generated = generated + random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        generated = generated + random.choice("0123456789")
+        generated = generated + random.choice("0123456789")
+        generated = generated + random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        generated = generated + random.choice("abcdefghijklmnopqrstuvwxyz")
+        generated = generated + "#"
+        generated = generated + "&"
+        if generated in table:
+            generated = ""
+        else:
+            return generated
 
-    return generated
+result = generate_random(table)
+print(result)
