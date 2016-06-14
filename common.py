624daefbccb00de880edd7298c0a97896005cb96
lorszil <lorszil@gmail.com>
2016-06-14 13:13:12 +0200

    add, remove, update function

--- common.py
+++ common.py
@@ -1,7 +1,11 @@
 # implement commonly used functions here
 
-import random
+table = [["kH34Ju", "Age of Empires II: The Age of Kings", "Ensemble Studios", "32", "32"],
+["jH34Ju", "Age of Mythology", "Ensemble Studios", "40", "4"],
+["tH34Ju", "Age of Empires II: The Conquerors", "Ensemble Studios", "30", "3"]]
 
+import random
+import ui
 
 # generate and return a unique and random string
 # other expectation:
@@ -30,3 +34,38 @@ def generate_random(table):
 
 result = generate_random(table)
 print(result)
+
+
+def common_add(table, title_list):
+    new_id = [generate_random(table)]
+    new_list = []
+    new_add = ui.get_inputs("Give me an")
+    for i in table:
+        title_list[0] = title_list[i]
+        new_list.append(ui.get_inputs("Give me an" + title_list))
+        new_id.append(new_list[i])
+    table.append(new_line)
+    return table
+
+
+def common_remove(table, id_):
+    data_manager.get_table_from_file(file_name)
+    data_manager.write_table_to_file(file_name)
+    the_input = ui.get_inputs("Give me an ID: ")
+    while True:
+        for i in table:
+            if i[0] == the_input:
+                table.remove()
+                break
+            else:
+                ui.print_error_message("This ID dosn't exist.")
+                continu
+
+common_remove()
+
+
+
+
+
+# def common_update(table, title_list):
+    # ui.get_inputs
